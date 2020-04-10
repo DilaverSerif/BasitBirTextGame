905326ea35ad953e3d81548688210297160a0771
Dilaver Şerif <dilaver.serif@outlook.com>
2020-04-10 10:43:53 +0300

    yep

--- src/com/dilaver/Ayi.java
+++
@@ -1,8 +0,0 @@
-package com.dilaver;
-
-public class Ayi extends Dusman {
-    @Override
-    public void Konus() {
-        main.Konusmaci("YARIAYI: AAAAARRRRRRRRRRRRRRRRRRRRRRRR KAFANI KOPARICAM INSAN!");
-    }
-}
--- src/com/dilaver/Corona.java
+++
@@ -1,8 +0,0 @@
-package com.dilaver;
-
-public class Corona extends Dusman {
-    @Override
-    public void Konus() {
-        main.Konusmaci("CORONA: BU SABAH ELLERINI YIKADIN MI!");
-    }
-}
